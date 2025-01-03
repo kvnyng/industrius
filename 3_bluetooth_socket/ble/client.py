e607ad1c9e0122025c5eb745f533881f512444cd
kvnyng <kevin.yang.contact@gmail.com>
2025-01-03 17:51:57 -0500

    moved assets

--- 3_bluetooth_socket/ble/client.py
+++ 3_bluetooth_socket/ble/client.py
@@ -1,7 +1,8 @@
 import asyncio
-from bleak import BleakClient
+from bleak import BleakClient, BleakGATTCharacteristic
 
 # Define the target device's service UUID and characteristic UUID
+DEVICE_ADDRESS = "D0611E78-BBB4-4591-A5F8-487910AE4366"
 SERVICE_UUID = "A07498CA-AD5B-474E-940D-16F1FBE7E8CD"
 CHARACTERISTIC_UUID = "51FF12BB-3ED8-46E5-B4F9-D64E2FEC021B"
 
@@ -19,6 +20,9 @@ async def read_characteristic(device_address):
             print(f"Service {SERVICE_UUID} not found on the device.")
             return
 
+        # subscribe to the characteristic
+        await client.start_notify(CHARACTERISTIC_UUID, notification_handler)
+
         # Read the characteristic value
         try:
             value = await client.read_gatt_char(CHARACTERISTIC_UUID)
@@ -27,8 +31,9 @@ async def read_characteristic(device_address):
             print(f"Failed to read characteristic: {e}")
 
 
-# Replace "DEVICE_ADDRESS" with the MAC address of the BLE device you are connecting to.
-DEVICE_ADDRESS = "B8:27:EB:19:77:0A"
+def notification_handler(sender: BleakGATTCharacteristic, data: bytearray):
+    print(f"{sender}: {data}")
+
 
 if __name__ == "__main__":
     asyncio.run(read_characteristic(DEVICE_ADDRESS))
